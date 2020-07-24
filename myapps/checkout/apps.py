a8c000b6debd664f693cedbb67304fe3622219d4
mikhail.trunov <mikhail.trunov@gmail.com>
2020-07-24 22:03:26 +0400

    Try to set urls

--- myapps/checkout/apps.py
+++ myapps/checkout/apps.py
@@ -1,5 +1,19 @@
 import oscar.apps.checkout.apps as apps
 
+from django.urls import path
+from .views import ShippingMethodView
+
 
 class CheckoutConfig(apps.CheckoutConfig):
     name = 'myapps.checkout'
+
+    def ready(self):
+        super().ready()
+        self.ShippingMethodView = ShippingMethodView
+
+    def get_urls(self):
+        urls = super().get_urls()
+        urls += [
+            path(r'extra/$', self.ShippingMethodView.as_view(), name='extra'),
+        ]
+        return self.post_process_urls(urls)
