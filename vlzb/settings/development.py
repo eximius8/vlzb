a18c43ac1d117655ad3750474929842faef558f9
mikhail.trunov <mikhail.trunov@gmail.com>
2020-08-16 14:39:42 +0400

    Forked order app Added login credentials

--- vlzb/settings/development.py
+++ vlzb/settings/development.py
@@ -1,8 +1,23 @@
 from .base import *
 
+import os
+
 DEBUG = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+# http://michael-borisov.com/2014/10/09/yandex-smtp-and-django/
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = os.environ.get('BEZODER_EMAIL_SMTP')
+EMAIL_HOST_USER = os.environ.get('BEZODER_EMAIL') 
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = os.environ.get('BEZODER_EMAIL')
+
+EMAIL_HOST_PASSWORD = os.environ.get('BEZODER_EMAIL_PASSWORD')
 
 
 # Database
